7fcbff1c2bdc13703e8293904efdfe5825b35680
chappertron <52535659+chappertron@users.noreply.github.com>
2024-04-15 14:14:49 +0100

    (refactor) Extracted much of the main function into struct.

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,24 @@
 /// - [ ] Add a test suite
 /// - [ ] Create an issue abstraction to handle all errors and warnings
 /// - [ ] Extract core behaviour into a library
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 use clap::Parser as ClapParser;
+use lammps_analyser::issues::Issue as ScriptIssue;
 use lammps_analyser::{
-    ast::{ts_to_ast, CommandType, NamedCommand, PartialAst},
+    ast::{from_node::FromNodeError, ts_to_ast, Ast, CommandType, NamedCommand, PartialAst},
     check_commands,
     check_styles::check_styles,
     diagnostic_report::ReportSimple,
+    diagnostics::{Diagnostic, Issue},
     error_finder::ErrorFinder,
     identifinder::{unused_variables, IdentiFinder},
-    issues::Issue,
     lammps_errors::{LammpsError, Warnings},
+    spanned_error::SpannedError,
 };
 use owo_colors::OwoColorize;
-use std::{
-    fs::File,
-    io::{BufReader, Read},
-};
-use tree_sitter::Parser;
+use std::fs::File;
+use tree_sitter::{Parser, Tree};
 
 #[derive(Debug, clap::Parser)]
 struct Cli {
@@ -39,107 +38,138 @@ struct Cli {
     output_reports: bool,
 }
 
+#[derive(Debug)] // TODO: Allow for implementing clone. Can't yet because of Query in Identifinder.
+pub struct InputScript<'src> {
+    pub source_code: &'src str,
+    pub diagnostics: Vec<Diagnostic>,
+    pub issues: Vec<ScriptIssue>, // TODO: Use the new issue trait?
+    pub tree: Tree,
+    pub ast: Ast,
+    pub ast_errors: Option<Vec<SpannedError<FromNodeError>>>,
+    pub identifinder: IdentiFinder,
+    pub error_finder: ErrorFinder,
+}
+
+impl<'src> InputScript<'src> {
+    /// Monolithic method that reads the lammps source code.
+    /// Parser is taken as input rather than stored because it does not implement debug.
+    fn new(source_code: &'src str, parser: &mut Parser) -> Result<Self> {
+        let tree = parser
+            .parse(&source_code, None)
+            .context("Failed to load the TS grammar.")?;
+
+        let mut issues: Vec<ScriptIssue> = Vec::new();
+        let mut diagnostics: Vec<Diagnostic> = Vec::new();
+
+        let ast = ts_to_ast(&tree, &source_code);
+        // Somewhat gracefully exit
+
+        let (ast, ast_errors) = match ast {
+            Ok(ast) => (ast, None),
+            Err(PartialAst { ast, errors }) => (ast, Some(errors)),
+        };
+
+        // Checking fix arguments
+        let fix_errors = ast
+            .commands
+            .iter()
+            .filter_map(|command| {
+                if let CommandType::NamedCommand(NamedCommand::Fix(fix)) = &command.command_type {
+                    Some(check_commands::fixes::check_fix(fix))
+                } else {
+                    None
+                }
+            })
+            .filter_map(|x| x.err())
+            .map(|issue| issue.diagnostic())
+            .collect::<Vec<_>>();
+
+        let identifinder = IdentiFinder::new(&tree, &source_code)?;
+
+        let undefined_fixes = match identifinder.check_symbols() {
+            Ok(()) => vec![],
+            Err(v) => v,
+        };
+
+        let mut error_finder = ErrorFinder::new()?;
+        _ = error_finder.find_syntax_errors(&tree, &source_code)?;
+        error_finder.find_missing_nodes(&tree)?;
+        let syntax_errors = error_finder.syntax_errors();
+
+        // TODO: Check if any warnings or errors are found!!!
+
+        let invalid_styles = check_styles(&tree, &source_code)?;
+        issues.extend(
+            syntax_errors
+                .iter()
+                .map(|x| LammpsError::from(x.clone()).into()),
+        );
+        issues.extend(
+            undefined_fixes
+                .into_iter()
+                .map(|x| LammpsError::from(x).into()),
+        );
+        issues.extend(
+            invalid_styles
+                .into_iter()
+                .map(|x| LammpsError::from(x).into()),
+        );
+        issues.extend(
+            unused_variables(identifinder.symbols())
+                .into_iter()
+                .map(|x| Warnings::from(x).into()),
+        );
+
+        diagnostics.extend(fix_errors.into_iter());
+
+        Ok(Self {
+            source_code,
+            tree,
+            issues,
+            ast,
+            ast_errors,
+            diagnostics,
+            identifinder,
+            error_finder,
+        })
+    }
+}
+
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    let file = File::open(&cli.source)?;
-
-    let mut file = BufReader::new(file);
-
-    let mut source_code = String::new();
-
-    file.read_to_string(&mut source_code)?;
-
-    let source_bytes = source_code.as_bytes();
-
-    let mut issues: Vec<Issue> = Vec::new();
-
+    let source_code = std::fs::read_to_string(&cli.source)?;
     let mut parser = Parser::new();
 
     parser
         .set_language(tree_sitter_lammps::language())
-        .expect("Could not load language");
+        .context("Could not load tree-sitter language")?;
 
-    let tree = parser
-        .parse(source_bytes, None)
-        .expect("Failed to load the TS grammar.");
+    let state = InputScript::new(&source_code, &mut parser)?;
 
+    // Output a syntax tree for debugging.
     if cli.output_tree {
         let dot_file = File::create("tree.dot")?;
-        tree.print_dot_graph(&dot_file);
+        state.tree.print_dot_graph(&dot_file);
     }
 
-    let ast = ts_to_ast(&tree, source_bytes);
-
-    // Somewhat gracefully exit
-    if let Err(e) = &ast {
-        for error in &e.errors {
+    // Errors
+    if let Some(errors) = state.ast_errors {
+        for error in errors {
             println!("{}:{}", cli.source.bold(), error.make_simple_report());
         }
     }
-    let ast = match ast {
-        Ok(ast) => ast,
-        Err(PartialAst { ast, .. }) => ast,
-    };
 
-    // Checking fix arguments
-    let fix_errors = ast
-        .commands
-        .iter()
-        .filter_map(|command| {
-            if let CommandType::NamedCommand(NamedCommand::Fix(fix)) = &command.command_type {
-                Some(check_commands::fixes::check_fix(fix))
-            } else {
-                None
-            }
-        })
-        .filter_map(|x| x.err())
-        .collect::<Vec<_>>();
-
-    let identifinder = IdentiFinder::new(&tree, source_bytes)?;
-
-    let undefined_fixes = match identifinder.check_symbols() {
-        Ok(()) => vec![],
-        Err(v) => v,
-    };
-
-    let mut error_finder = ErrorFinder::new()?;
-    _ = error_finder.find_syntax_errors(&tree, source_bytes)?;
-    error_finder.find_missing_nodes(&tree)?;
-    let syntax_errors = error_finder.syntax_errors();
-
-    // TODO: Check if any warnings or errors are found!!!
-
-    let invalid_styles = check_styles(&tree, source_bytes)?;
-    issues.extend(
-        syntax_errors
-            .iter()
-            .map(|x| LammpsError::from(x.clone()).into()),
-    );
-    issues.extend(
-        undefined_fixes
-            .into_iter()
-            .map(|x| LammpsError::from(x).into()),
-    );
-    issues.extend(
-        invalid_styles
-            .into_iter()
-            .map(|x| LammpsError::from(x).into()),
-    );
-    issues.extend(
-        unused_variables(identifinder.symbols())
-            .into_iter()
-            .map(|x| Warnings::from(x).into()),
-    );
-
-    issues.extend(fix_errors.into_iter().map(|x| LammpsError::from(x).into()));
-
-    for issue in &issues {
+    for issue in &state.issues {
         println!("{}", issue.make_simple_report());
     }
-    if !issues.is_empty() {
+
+    for diagnostic in &state.diagnostics {
+        println!("{}", diagnostic.make_simple_report());
+    }
+    if !state.issues.is_empty() && !state.diagnostics.is_empty() {
         // TODO:  Don't count warnings as errors!!!
-        let n_errors = issues.len();
+        let n_errors = state.issues.len() + state.diagnostics.len();
         println!(
             "{}: {} error{} found 😞",
             cli.source.bold(),
