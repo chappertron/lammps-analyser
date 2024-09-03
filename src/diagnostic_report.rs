ebba02d3804d89e051c7029d27e79faa1ac82fe0
chappertron <52535659+chappertron@users.noreply.github.com>
2024-09-03 15:43:39 +0100

    fix: removed unused dependancy ariadne and trait.
    BREAKING: removed the `ReportDiagnostic` trait

--- src/diagnostic_report.rs
+++ src/diagnostic_report.rs
@@ -1,17 +1,5 @@
 use std::fmt::Display;
 
-use ariadne::{Report, Span};
-// use std::fmt::Debug;
-// use std::hash::Hash;
-
-/// Convert an error into a report that can be outputted into terminal
-pub trait ReportDiagnostic<S: Span> {
-    fn make_report(&self, source_id: &str) -> Report<'_, S>;
-    // where
-    //     Id: Debug + Hash + PartialEq + Eq + ToOwned,
-    //     <Id as ToOwned>::Owned: From<Id>;
-}
-
 /// Trait to impl on errors for colourising the output
 /// Meant to be for simple output to the commandline
 pub trait ReportSimple {
